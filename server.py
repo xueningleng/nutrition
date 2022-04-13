28d61ecf6f76073ef766462bc7d4c30b6adcffd8
xueningleng <sherryleng27@gmail.com>
2022-04-13 17:05:39 -0400

    Quiz data moved to server

--- server.py
+++ server.py
@@ -5,6 +5,98 @@ app = Flask(__name__)
 # DATA
 quiz_progress = 1
 quiz_score = 0
+quiz_questions = [
+    {
+        'question-type':"drag-n-drop",
+        'prompt':"Drag each item into their category",
+        'categories': [
+            'High Protein',
+            'High Carbs',
+            'High Fat',
+        ],
+        'choices':[
+            "Chicken",
+            "Rice",
+            "Beef",
+            "Pork Belly",
+            "Oats",
+            "Fish",
+        ],
+        'correct_answer':{
+            'High Protein' : ["Chicken","Beef","Fish",],
+            'High Carbs' : ["Rice", "Oats",],
+            'High Fat' : ["Pork Belly",],
+        }
+
+    },
+    {
+        'question-type':"drag-n-drop",
+        'prompt':"Drag each item into their category",
+        'categories': [
+            'Healthy',
+            'Unhealthy',
+        ],
+        'choices':[
+            "Triple deep-fried pork belly",
+            "French Fries",
+            "Air-fried chicken strips",
+            "Donuts",
+            "Whole-grained protein bars",
+            "Strawberry smoothies",
+            "Matcha Boba tea with 25% sweetness",
+        ],
+        'correct_answer': {
+            'Healthy' : [
+                "Air-fried chicken strips",
+                "Matcha Boba tea with 25% sweetness",
+                "Strawberry smoothies",
+                "Whole-grained protein bars",
+            ],
+            'Unhealthy' : [
+                "Triple deep-fried pork belly",
+                "French Fries",
+                "Donuts",
+            ],
+        }
+    },
+    {
+        'question-type':"multiple-choice",
+        'prompt':"Select the best answer",
+        'question':"Ricky has consumed 45 grams of unsaturated fats, "+
+            "213 grams of sugars, " +
+            "40 grams of fibers, 80 grams of " +
+            "protein. How much calories has Ricky " +
+            "consumed?",
+        'answers': [1737, 1576, 1720, 1832],
+        'correct':  1737,
+        'image': "http://justfunfacts.com/wp-content/uploads/2021/03/junk-food.jpg"
+    },
+    {
+        'question-type':"multiple-choice",
+        'prompt':"Select the best answer",
+        'question':"Jane just finished her workout and " +
+            "according to her fitbit, she has burnt 670 " +
+            "calories! To celebrate this achievement, " +
+            "she decides to order McDonald's. She " +
+            "wants to order a customized burger " +
+            "which has 60 grams of protein, 20 " +
+            "grams of fat, and 40 grams of carbs. " +
+            "After this meal, how many effective calories are" +
+            " actually burnt from her workout?",
+        'answers': [82, 95, 97, 100],
+        'correct': 95,
+        'image': "https://www.verywellfit.com/thmb/LOOS3DrPc3hC0ZaHMsd3GMfT-wI=/1500x1000/filters:fill(FFDB5D,1)/which-at-home-workout-options-are-right-for-you-5113667_static-4cfe40ec3ebe49a886353d25c4457886.png"
+    },
+    {
+        'question-type':"multiple-choice",
+        'prompt':"Select the best answer",
+        'question': "Given this food label, what is the total\n" +
+            "calories if Samantha ate two yogurts today as her dinner?",
+        'answers':[70, 85, 170, 340],
+        'correct': 340,
+        'image':"https://sites.google.com/a/g.coppellisd.com/nutrition-p7-c-gonzalez/_/rsrc/1348173855890/food-label/Yoplait_Original_Strawberry_Lemonade.jpg"
+    }
+]
 # ROUTES
 
 @app.route('/')
@@ -18,7 +110,7 @@ def overview():
 
 @app.route('/quiz')
 def quiz():
-   return render_template('quiz.html', data={'quiz_progress':quiz_progress, 'quiz_score':quiz_score})
+   return render_template('quiz.html', data={'quiz_progress':quiz_progress, 'quiz_score':quiz_score, 'quiz_questions':quiz_questions})
 
 
 
--- static/quiz.js
+++ static/quiz.js
@@ -1,95 +1,3 @@
-const questions = [
-    {
-        'question-type':"drag-n-drop",
-        'prompt':"Drag each item into their category",
-        'categories': [
-            'High Protein',
-            'High Carbs',
-            'High Fat',
-        ],
-        'choices':[
-            "Chicken",
-            "Rice",
-            "Beef",
-            "Pork Belly",
-            "Oats",
-            "Fish",
-        ],
-        'correct_answer':{
-            'High Protein' : ["Chicken","Beef","Fish",],
-            'High Carbs' : ["Rice", "Oats",],
-            'High Fat' : ["Pork Belly",],
-        }
-
-    },
-    {
-        'question-type':"drag-n-drop",
-        'prompt':"Drag each item into their category",
-        'categories': [
-            'Healthy',
-            'Unhealthy',
-        ],
-        'choices':[
-            "Triple deep-fried pork belly",
-            "French Fries",
-            "Air-fried chicken strips",
-            "Donuts",
-            "Whole-grained protein bars",
-            "Strawberry smoothies",
-            "Matcha Boba tea with 25% sweetness",
-        ],
-        'correct_answer': {
-            'Healthy' : [
-                "Air-fried chicken strips",
-                "Matcha Boba tea with 25% sweetness",
-                "Strawberry smoothies",
-                "Whole-grained protein bars",
-            ],
-            'Unhealthy' : [
-                "Triple deep-fried pork belly",
-                "French Fries",
-                "Donuts",
-            ],
-        }
-    },
-    {
-        'question-type':"multiple-choice",
-        'prompt':"Select the best answer",
-        'question':"Ricky has consumed 45 grams of unsaturated fats, "+
-            "213 grams of sugars, " +
-            "40 grams of fibers, 80 grams of " +
-            "protein. How much calories has Ricky " +
-            "consumed?",
-        'answers': [1737, 1576, 1720, 1832],
-        'correct':  1737,
-        'image': "http://justfunfacts.com/wp-content/uploads/2021/03/junk-food.jpg"
-    },
-    {
-        'question-type':"multiple-choice",
-        'prompt':"Select the best answer",
-        'question':"Jane just finished her workout and " +
-            "according to her fitbit, she has burnt 670 " +
-            "calories! To celebrate this achievement, " +
-            "she decides to order McDonald's. She " +
-            "wants to order a customized burger " +
-            "which has 60 grams of protein, 20 " +
-            "grams of fat, and 40 grams of carbs. " +
-            "After this meal, how many effective calories are" +
-            " actually burnt from her workout?",
-        'answers': [82, 95, 97, 100],
-        'correct': 95,
-        'image': "https://www.verywellfit.com/thmb/LOOS3DrPc3hC0ZaHMsd3GMfT-wI=/1500x1000/filters:fill(FFDB5D,1)/which-at-home-workout-options-are-right-for-you-5113667_static-4cfe40ec3ebe49a886353d25c4457886.png"
-    },
-    {
-        'question-type':"multiple-choice",
-        'prompt':"Select the best answer",
-        'question': "Given this food label, what is the total\n" +
-            "calories if Samantha ate two yogurts today as her dinner?",
-        'answers':[70, 85, 170, 340],
-        'correct': 340,
-        'image':"https://sites.google.com/a/g.coppellisd.com/nutrition-p7-c-gonzalez/_/rsrc/1348173855890/food-label/Yoplait_Original_Strawberry_Lemonade.jpg"
-    }
-]
 var correct = true;
 
 var drop_lists = {
@@ -116,7 +24,6 @@ const correct_dict = {
     "Triple deep-fried pork belly": 'Unhealthy',
     "French Fries": 'Unhealthy',
     "Donuts": 'Unhealthy',
-
 }
 
 function end_template(q_score){
@@ -307,6 +214,7 @@ function check_choice(q_info, selected){
 
 $(document).ready(function(){
     let q_num = quiz['quiz_progress']
+    let questions = quiz['quiz_questions']
     let q_info = questions[q_num-1]
     if (q_num<=5){
         if (q_info['question-type'] == 'drag-n-drop'){
