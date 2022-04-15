8f9aba525262eb9abf5be57bd502330db9597db5
Jinhyuk Kim <jikim@g.hmc.edu>
2022-04-15 02:32:00 -0400

    hover info working for overview

--- static/nutrition_facts.js
+++ static/nutrition_facts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('.map').maphilight();
- });
+    map_areas = ['#daily-value-rec', "#calories-rec", "#serving-info-rec", "#nutrients1-rec", "nutrients2-rec"]
+    for (var i = 0; i < 5; i++) {
+        $(map_areas[i]).mouseout(function(){
+            $("#description-text").replaceWith( ('<div class = "description-text">  </div>') );
+        })
+        $(map_areas[i]).mouseover(function(){
+            $("#description-text").html( ('<div class = "description-text">' + map_areas[i] + '</div>') );
+        })
+        consonle.log(map_areas[i])
+    }
+});
