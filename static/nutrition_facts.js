e1894281e9b27a0830f4c4f462676e726c3b399f
Jinhyuk Kim <jikim@g.hmc.edu>
2022-04-17 17:49:35 -0400

    hover fixed for all regions

--- static/nutrition_facts.js
+++ static/nutrition_facts.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
     $('.map').maphilight();
-    map_areas = ['#daily-value-rec', "#calories-rec", "#serving-info-rec", "#nutrients1-rec", "nutrients2-rec"]
-    for (var i = 0; i < 5; i++) {
-        $(map_areas[i]).mouseout(function(){
-            $("#description-text").replaceWith( ('<div class = "description-text">  </div>') );
-        })
-        $(map_areas[i]).mouseover(function(){
-            $("#description-text").html( ('<div class = "description-text">' + map_areas[i] + '</div>') );
-        })
-        consonle.log(map_areas[i])
-    }
+    var map_areas = ['#daily-value-rec', "#calories-rec", "#serving-info-rec", "#nutrients1-rec", "#nutrients2-rec"];
+    $('#daily-value-rec').mouseout(function(){
+        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    })
+    $('#daily-value-rec').mouseover(function(){
+        var s = '<div id="description-text">' + '#daily-value-rec' + '</div>'
+        console.log(s)
+        $("#description-text").html(s);
+    })
+    $('#calories-rec').mouseout(function(){
+        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    })
+    $('#calories-rec').mouseover(function(){
+        var s = '<div id="description-text">' + '#calories-rec' + '</div>'
+        $("#description-text").html(s);
+    })
+    $('#serving-info-rec').mouseout(function(){
+        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    })
+    $('#serving-info-rec').mouseover(function(){
+        var s = '<div id="description-text">' + '#serving-info-rec' + '</div>'
+        $("#description-text").html(s);
+    })
+    $('#nutrients1-rec').mouseout(function(){
+        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    })
+    $('#nutrients1-rec').mouseover(function(){
+        var s = '<div id="description-text">' + '#nutrients1-rec' + '</div>'
+        $("#description-text").html(s);
+    })
+    $('#nutrients2-rec').mouseout(function(){
+        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    })
+    $('#nutrients2-rec').mouseover(function(){
+        var s = '<div id="description-text">' + '#nutrients2-rec' + '</div>'
+        $("#description-text").html(s);
+    })
 });
