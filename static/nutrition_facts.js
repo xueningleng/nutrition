cddc2c8eb9c9ac585ae496166694e4b76fc7545f
Mausam Agrawal <mausam2906@gmail.com>
2022-04-18 12:13:28 -0400

    Hover functionality

--- static/nutrition_facts.js
+++ static/nutrition_facts.js
@@ -27,42 +27,42 @@ $(document).ready(function(){
     //         console.log("An error has occurred.");
     // });
     var map_areas = ['#daily-value-rec', "#calories-rec", "#serving-info-rec", "#nutrients1-rec", "#nutrients2-rec"];
-    $('#daily-value-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
-    })
+    // $('#daily-value-rec').mouseout(function(){
+    //     $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    // })
     $('#daily-value-rec').mouseover(function(){
         var s = '<div id="description-text">' + '#daily-value-rec' + '</div>'
         console.log(s)
         $("#description-text").html(s);
     })
 
-    $('#calories-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
-    })
+    // $('#calories-rec').mouseout(function(){
+    //     $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    // })
     $('#calories-rec').mouseover(function(){
-        var s = '<div id="description-text">' + '#calories-rec' + '</div>'
+        var s = '<div id="description-text">' + 'Calorie: the energy needed to raise the temperature of 1 gram of water through 1°C' + '</div>'
         $("#description-text").html(s);
     })
 
-    $('#serving-info-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
-    })
+    // $('#serving-info-rec').mouseout(function(){
+    //     $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    // })
     $('#serving-info-rec').mouseover(function(){
         var s = '<div id="description-text">' + '#serving-info-rec' + '</div>'
         $("#description-text").html(s);
     })
     
-    $('#nutrients1-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
-    })
+    // $('#nutrients1-rec').mouseout(function(){
+    //     $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    // })
     $('#nutrients1-rec').mouseover(function(){
         var s = '<div id="description-text">' + '#nutrients1-rec' + '</div>'
         $("#description-text").html(s);
     })
 
-    $('#nutrients2-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
-    })
+    // $('#nutrients2-rec').mouseout(function(){
+    //     $('#description-text').html( ('<div id="description-text">  Hover over something to learn about it  </div>') );
+    // })
     $('#nutrients2-rec').mouseover(function(){
         var s = '<div id="description-text">' + '#nutrients2-rec' + '</div>'
         $("#description-text").html(s);
