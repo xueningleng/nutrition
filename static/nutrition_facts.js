3ad9b02c313c808cbc6e9a9aad9c25a87104663b
benningtonli <bennington.lee@gmail.com>
2022-05-02 02:54:48 -0400

    Changed Image URL links for snacks Edited Food label info styling

--- static/nutrition_facts.js
+++ static/nutrition_facts.js
@@ -1,11 +1,12 @@
 var label_data = {
     "1": {
     "descriptions": ["<span class='text-bold'>Serving sizes are standardized in familiar units</span>, such as cups or pieces, followed by the metric amount, e.g., the number of grams (g)",
-        "It is not a recommendation of how much you should eat or drink but an <span class='text-bold'>indicator of the amount that people typically eat or drink</span>"]
+        "It is <span class='text-bold'>not</span> a recommendation of how much you should eat or drink but an <span class='text-bold'>indicator of the amount that people typically eat or drink</span>"]
     },
     "2": {
         "descriptions": ["<span class='text-bold'>Calories</span> provide a measure of how much <span class='text-bold'>energy you get from a serving of this food</span>.",
-            "<span class='text-bold'>Remember</span>: The number of servings you consume determines the number of calories you actually eat. <span class='text-bold'>Eating too many calories per day is linked to overweight and obesity</span>."]
+            "<span class='text-bold'>Remember</span>: The number of servings you consume determines the number of calories you actually eat. <span class='text-bold'>Eating too many calories per day is linked to overweight and obesity</span>.",
+        "To achieve or maintain a healthy body weight, balance the number of calories you eat and drink with the number of calories your body uses."]
     },
     "3": {
         "descriptions": ["<span class='text-bold'>Cholesterol: waxy, fat-like substance</span> found in all cells of the body. <span class='text-bold'>Higher in dietary cholesterol ~= higher in saturated fat</span>. Should keep this as low as possible",
@@ -18,117 +19,172 @@ var label_data = {
             "<span class='text-bold'>5% DV -> Low</span>",
             "<span class='text-bold'>20% DV -> High</span>"]
     },
-    "5": {
-        "descriptions": ["Fat: <span class='text-bold'>1 g fat = 9 cals</span>",""]
-    }
 }
+
+function revertDefault() {
+    $('#description-text').html("");
+    $('#hover-text').html("Hover over something to learn about it");
+    $('#hover-text').removeClass("hover-text-title");
+    $('#hover-text').addClass("hover-text-default");
+}
+
+function setTitle(newTitle) {
+    $('#hover-text').html(newTitle);
+    $('#hover-text').removeClass("hover-text-default");
+    $('#hover-text').addClass("hover-text-title");
+}
+
+function display(index) {
+    $.each(label_data[index]["descriptions"], function() {
+        $('#description-text').append($('<li>').html(this));
+    });
+}
+
 $(document).ready(function(){
     $('.map').maphilight({
         fillColor: 'fff9c2',
         fillOpacity: 0.5,
         strokeColor: '5cb25d',
     });
-
-    let j = label_data
+    revertDefault()
     var map_areas = ['#daily-value-rec', "#calories-rec", "#serving-info-rec", "#nutrients1-rec", "#nutrients2-rec"];
     $('#daily-value-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#daily-value-rec').mouseover(function(){
-        $.each(j["4"]["descriptions"], function() {
-            $('#description-text').append($('<li>').html(this));
-        });
-        $(".text-bold").css("color", "black");
-        // var s = '<div id="description-text">' + j["4"]["descriptions"] + '</div>'
-        // console.log(s)
-        // $("#description-text").html(s);
+        setTitle("The Percent Daily Value")
+        display("4")
     })
 
     $('#calories-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
+
     })
     $('#calories-rec').mouseover(function(){
-        $.each(j["2"]["descriptions"], function() {
-            $('#description-text').append($('<li>').html(this));
-        });
-        $(".text-bold").css("color", "black");
+        setTitle("Calories Information")
+        display("2")
+
         // var s = '<div id="description-text">' + j["2"]["descriptions"] + '</div>'
         // $("#description-text").html(s);
     })
 
     $('#serving-info-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#serving-info-rec').mouseover(function(){
-        $.each(j["1"]["descriptions"], function() {
-            $('#description-text').append($('<li>').html(this));
-        });
-        $(".text-bold").css("color", "black");
+        setTitle("Serving Information")
+        display("1")
+
         // var s = '<div id="description-text">' +  j["1"]["descriptions"] + '</div>'
         // $("#description-text").html(s);
     })
     
     $('#nutrients1-rec-fat').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients1-rec-fat').mouseover(function(){
-        var fat = '<span class="text-bold">Fat<br>1 g fat = 9 cals</span> <ul type="disc"><li><span class="text-bold">Saturated Fat: Bad fat</span> <ul type="circle"> <li>Most saturated fats are animal fats</li> <li>Too much saturated fat ~= increase blood cholesterol levels and bad cholesterol levels.</li><li>higher saturated fat intake ~= increased heart disease risks</li> </ul></li><li><span class="text-bold">Saturated Fat: Bad fat</span> <ul type="circle"> <li>Byproduct of hydrogenation that is used to turn healthy oils into solids</li> <li>Most Found in fried foods and baked goods</li><li>Trans fats ~= increased risk of inflammation</li> </ul></li><li><span class="text-bold">Nutrients to get less of: both</span></li></ul> '
-        var s = '<div id="description-text">' +  fat + '</div>'
-        $("#description-text").html(s);
+        setTitle("Fat")
+        let fat =
+        '<h4>1 g fat = 9 cals</h4>' +
+            '<ul type="disc">' +
+                '<li><span class="text-bold">Saturated Fat: Bad fat</span> ' +
+                    '<ul type="circle"> ' +
+                        '<li>Most saturated fats are animal fats</li> ' +
+                        '<li>Too much saturated fat ~= increase blood cholesterol levels and bad cholesterol levels.</li>' +
+                        '<li>higher saturated fat intake ~= increased heart disease risks</li> ' +
+                    '</ul>' +
+                '</li>' +
+                '<li><span class="text-bold">Saturated Fat: Bad fat</span> ' +
+                    '<ul type="circle"> ' +
+                        '<li>Byproduct of hydrogenation that is used to turn healthy oils into solids</li>' +
+                        '<li>Most Found in fried foods and baked goods</li>' +
+                        '<li>Trans fats ~= increased risk of inflammation</li> ' +
+                    '</ul>' +
+                '</li>' +
+                '<li><span class="text-bold">Nutrients to get less of: both</span>' +
+                '</li>' +
+            '</ul> '
+        $("#description-text").html(fat);
         $(".text-bold").css("color", "black");
     })
 
     $('#nutrients1-rec-carb').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients1-rec-carb').mouseover(function(){
-        var carb = '<span class="text-bold">Carbs<br>1 g carb = 4 cals</span> <ul type="disc"><li><span class="text-bold">Total Sugars: natural sugars + added sugars</span><ul type="circle"> <li>Natural sugars: sugars in fruits</li> <li>Added Sugars: cane sugars</li></ul></li><li><span class="text-bold">Dietary Fiber: Indigestible part = healthy</span><ul type="circle"><li>Protect against heart disease</li><li>Better gut health</li></ul></li><li><span class="text-bold">Nutrients to get more of: Dietary Fiber</span></li><li><span class="text-bold">Nutrients to get less of: Added sugars</span></li></ul>  '
-        var s = '<div id="description-text">' +  carb + '</div>'
-        $("#description-text").html(s);
+        setTitle("Carb")
+
+        let carb =
+            '<h4>1 g carb = 4 cals</h4>' +
+            '<ul type="disc">' +
+                '<li><span class="text-bold">Total Sugars: natural sugars + added sugars</span>' +
+                    '<ul type="circle"> ' +
+                        '<li>Natural sugars: sugars in fruits</li>' +
+                        '<li>Added Sugars: cane sugars</li>' +
+                    '</ul>' +
+                '</li>' +
+                '<li><span class="text-bold">Dietary Fiber: Indigestible part = healthy</span>' +
+                    '<ul type="circle">' +
+                        '<li>Protect against heart disease</li>' +
+                        '<li>Better gut health</li>' +
+                    '</ul>' +
+                '</li>' +
+                '<li><span class="text-bold">Nutrients to get more of: Dietary Fiber</span></li>' +
+                '<li><span class="text-bold">Nutrients to get less of: Added sugars</span></li>' +
+            '</ul>  '
+        $("#description-text").html(carb);
         $(".text-bold").css("color", "black");
     })
 
     $('#nutrients1-rec-protein').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients1-rec-protein').mouseover(function(){
-        var carb = '<span class="text-bold">Protein<br>1 g protein = 4 cals</span> <ul type="disc"><li>Found in <span class="text-bold">beans peas, dairy products, meat, nuts seafood, whole grains and vegetables</span> etc</li><li>Necessary for <span class="text-bold">proper growth and development</span></li><li>Build and repairs body tissues</li><li>Important for many body processes</li><li>Nutrients to get more of: Protein</li></ul>'
-        var s = '<div id="description-text">' +  carb + '</div>'
-        $("#description-text").html(s);
-        $(".text-bold").css("color", "black");
+        setTitle("Protein")
+        let protein =
+        '<h4>1 g protein = 4 cals</h4>' +
+            '<ul type="disc">' +
+                '<li>Found in <span class="text-bold">beans peas, dairy products, meat, nuts seafood, whole grains and vegetables</span> etc</li>' +
+                '<li>Necessary for <span class="text-bold">proper growth and development</span></li>' +
+                '<li>Build and repairs body tissues</li><li>Important for many body processes</li>' +
+                '<li>Nutrients to get more of: Protein</li></ul>'
+        $("#description-text").html(protein);
     })
 
     $('#nutrients1-rec-chol-sodium').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients1-rec-chol-sodium').mouseover(function(){
-        $.each(j["3"]["descriptions"], function() {
-            $('#description-text').append($('<li>').html(this));
-        });
-        $(".text-bold").css("color", "black");
+        display("3")
+
         // var carb = 'Protein<br>1 g protein = 4 cals <ul type="disc"><li>Found in beans peas, dairy products, meat, nuts seafood, whole grains and vegetables etc</li><li>Necessary for proper growth and development</li><li>Build and repairs body tissues</li><li>Important for many body processes</li><li>Nutrients to get more of: Protein</li></ul>'
         // var s = '<div id="description-text">' +  carb + '</div>'
         // $("#description-text").html(s);
     })
     $('#nutrients1-vit-min').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients1-vit-min').mouseover(function(){
-        var vit_min = '<span class="text-bold">Vitamins and minerals</span><br><ul><li>Diets rich in vitamins and minerals <span class="text-bold">promote growth, development, and normal body functioning</span></li><li><span class="text-bold">Reduce the risk of developing osteoporosis</span>, anemia, and high blood pressure, respectively</li></ul'
-        var s = '<div id="description-text">' +  vit_min + '</div>'
-        $("#description-text").html(s);
-        $(".text-bold").css("color", "black");
+        setTitle("Vitamins and Minerals")
+        let vit_min =
+            '<ul>' +
+                '<li>Diets rich in vitamins and minerals <span class="text-bold">promote growth, development, and normal body functioning</span></li>' +
+                '<li><span class="text-bold">Reduce the risk of developing osteoporosis</span>, anemia, and high blood pressure, respectively</li>' +
+            '</ul'
+        $("#description-text").html(vit_min);
     })
 
     $('#nutrients2-rec').mouseout(function(){
-        $('#description-text').html( ('<div id="description-text"></div>') );
+        revertDefault();
     })
     $('#nutrients2-rec').mouseover(function(){
-        var nutrient_breakdown = '<span class="text-bold">Nutrients breakdowns</span><br><ul><li>Now, lets focus on understanding the significance of these numbers and <span class="text-bold">how each macro contribute to the total calories!</span></li></ul>'
-        var s = '<div id="description-text">' + nutrient_breakdown + '</div>'
-        $("#description-text").html(s);
+        setTitle("Nutrition Recommendation")
+        let nutrient_recs =
+        '<ul>' +
+            '<li>These numbers are your daily recommendations of how much each macro you should consume.</li>' +
+        '</ul>'
+        $("#description-text").html(nutrient_recs);
         $(".text-bold").css("font-weight", "100");
         // $(".text-bold").css("font-style", "italic");
-        $(".text-bold").css("color", "black");
     })
 });
--- static/snacks.js
+++ static/snacks.js
@@ -1,43 +1,47 @@
-imgs_and_labels = {
+var imgs_and_labels = {
     "Vegetable-Based Snacks": {
-        "mini carrots": {
+        "Mini carrots": {
             "image": "https://static.onecms.io/wp-content/uploads/sites/19/2017/04/04/GettyImages-121088095-2000.jpg",
             "label": "https://www.crystalvalleyfoods.com/wp-content/uploads/2017/05/Baby-Carrots-Nutrition-Label.png"
         },
-        "crunch dried veggies": {
+        "Crunch dried veggies": {
             "image": "https://nuts.com/images/rackcdn/ed910ae2d60f0d25bcb8-80550f96b5feb12604f4f720bfefb46d.ssl.cf1.rackcdn.com/382df9245a0a225b-dpcS2YMa-large.jpg",
-            "label": "https://m.media-amazon.com/images/I/71mDjaYJDJL._SL1500_.jpg"
+            // "label": "https://m.media-amazon.com/images/I/71mDjaYJDJL._SL1500_.jpg"
+            "label": "https://esha.com/wp-content/uploads/2015/07/label-blog2.png"
         },
     },
     "Fruit-Based Snacks": {
-        "fruits": {
+        "Fruits": {
             "image": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg",
             "label": "https://usapears.org/wp-content/uploads/2020/09/Nutrition-Facts-2020-340x533-1.png"
         },
 
-        "fruit bites": {
+        "Fruit bites": {
             "image": "https://cdn.shopify.com/s/files/1/0048/8544/6759/files/Home_VeggieFruitBites-Hero_1800x.jpg?v=1642701183",
-            "label": "https://cdn.shopify.com/s/files/1/0048/8544/6759/products/CherrySweetPotato_Nutrition_1400x.jpg?v=1642735223"
+            // "label": "https://cdn.shopify.com/s/files/1/0048/8544/6759/products/CherrySweetPotato_Nutrition_1400x.jpg?v=1642735223"
+            "label": "https://welchsfruitsnacks.com/wp-content/uploads/2018/08/30g-4-320x550.png"
         },
-        "fruit chips": {
+        "Fruit chips": {
             "image": "https://i5.walmartimages.com/asr/40173714-0d5c-4db4-9b03-2e02d0d0a500_2.e6a71418e7460e49afffccf196428098.jpeg",
-            "label": "https://images-na.ssl-images-amazon.com/images/I/51R2aIQfJ4L.jpg"
+            // "label": "https://images-na.ssl-images-amazon.com/images/I/51R2aIQfJ4L.jpg"
+            "label": "https://cdn.shopify.com/s/files/1/0087/6065/5938/products/fruit-snacks-nut_2000x.png?v=1616425468"
         }
     },
     "High-Fiber Snacks": {
-        "oats with protein powder": {
+        "Oats with protein powder": {
             "image": "https://skinnyfitalicious.com/wp-content/uploads/2017/12/protein-oatmeal-img3.jpg",
             "label": "https://cdn.shopify.com/s/files/1/0052/6657/1334/files/Banana.png?v=1602691258"
         },
-        "nuts": {
+        "Nuts": {
             "image": "https://www.verywellhealth.com/thmb/45PJfywZmkzbmoi3CpiA8aPlRS4=/2560x1920/smart/filters:no_upscale()/the-tree-nut-allergy-diet-guide-1324280-selects-25c863756406496d8796feacd58461c7.jpg",
-            "label": "https://najlas.com/wp-content/uploads/2018/11/Gone-Nuts-4-oz-Nutrition-Label.jpg"
+            // "label": "https://najlas.com/wp-content/uploads/2018/11/Gone-Nuts-4-oz-Nutrition-Label.jpg"
+            "label": "https://mocafeusa.com/wp-content/uploads/2019/07/Nuts-Seeds-Nutrition-Facts-Label.png"
         },
-        "oats bar": {
+        "Oats bar": {
             "image": "https://marisamoore.com/wp-content/uploads/2020/04/GF-Vegan-Oat-Bars-Recipe-1.jpg",
             "label": "https://chocolatecoveredkatie.com/wp-content/uploads/2016/02/image-1.png"
         },
-        "protein bar": {
+        "Protein bar": {
             "image": "https://m.media-amazon.com/images/I/71A7mTrAaRS._SL1500_.jpg",
             "label": "https://proteinpowder.com/wp-content/uploads/2019/02/PureBarChocPeaButt.png"
         }
@@ -55,15 +59,15 @@ $(document).ready(function(){
         $(".next_button").addClass("disappear")
     }
    
-    var build_examples = "Examples: "
-    for (var i = 0; i < j["hover_examples"].length; i++) {
-        p = j["hover_examples"][i]
+    let build_examples = "Examples: "
+    for (let i = 0; i < j["hover_examples"].length; i++) {
+        let p = j["hover_examples"][i]
         if (i < j["hover_examples"].length-1) {
             p += ", "
         } else {
             p += "  "
         }
-        var html_new = '<a class="hover-snacks">'+ p +'</a>'
+        let html_new = '<a class="hover-snacks">'+ p +'</a>'
         build_examples += html_new
     }
     $('.hover-examples').html(build_examples)
@@ -82,7 +86,7 @@ $(document).ready(function(){
                 let ex_img = $("<div class=col-md-6 example-image>")
                 $(ex_img).html('<img src="' + img_link + '" id="example-image" class="snack-image-container" alt="Example Snack Image">');
                 let nut_lab = $("<div class=\"col-md-6 nutrition-label\">")
-                $(nut_lab).html('<img src="' + label_link + '" id="nutrition-label" class="snack-image-container" alt="Label for Example Snack Image">');
+                $(nut_lab).html('<img src="' + label_link + '" id="nutrition-label" width="130%" class="snack-image-container" alt="Label for Example Snack Image">');
                 $(ex_img).appendTo($('#snack-image'))
                 $(nut_lab).appendTo($('#snack-image'))
 
